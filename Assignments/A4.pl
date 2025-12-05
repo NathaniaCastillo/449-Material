1ef034fb7f0c7f9d0c2f07645c7a1bcd7722c46e
nathania <nathania.castillo@ucalgary.ca>
2025-12-04 17:44:50 -0700

    question 1 queries are true expect for shared pet does not return the right answer

--- Assignments/A4.pl
+++ Assignments/A4.pl
@@ -1,20 +1,14 @@
-{-
+/*
 Nathania Castillo
 UCID:30152619
 Dr. Nasri
 Dec5, 2025 
 
 Assignment 4:
--}
-
-% Prob 1
-/*Facts:
-owns(alice, fluffy).
-owns(alice,max).
-owns(Bob, Bella).
-owns(carol, max).
-owns(carol, daisy).
 */
+
+% Prob 1 Pet Ownership
+% Facts:
 
 pet(fluffy).
 pet(max).
@@ -25,14 +19,28 @@ person(alice).
 person(bob).
 person(carol).
 
-% finds out the what person owns which pet 
-owns (Person, Pet):- person(Person), pet(Pet)
+owns(alice, fluffy).
+owns(alice, max).
+owns(bob, bella).
+owns(carol, daisy).
+owns(carol, max). 
 
+/* % finds out the what person owns which pet 
+owns(Person, Pet):- person(Person), pet(Pet).
+*/
 
+% find out who has a shared pet 
+shared_pet(Person1, Person2, Pet):- 
+owns(Person1, Pet), owns(Person2, Pet), 
+Person1 \= Person2.    % I get an error wityh
 
+% a person has more than one pet
+has_multiple_pets(Person):-
+owns(Person, Pet1), owns(Person, Pet2),
+Pet1 \= Pet2.  
 
-
-
+% Prob 2 List Comprehension
+% Facts:
 
 
 
